b86cf61fd1826f40585698ba9b5a282281189ed3
Jacob Dimmitt <jacdimmitt@gmail.com>
2021-08-21 11:25:15 -0500

    Added React component & CSS

---
+++ PathfindingVisualizer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PathfindingVisualizer = () => {
+    return (
+        <div id="pathfinder-body">
+            <h1>YO LET'S GO!</h1>
+        </div>
+    )
+}
+
+export default PathfindingVisualizer
